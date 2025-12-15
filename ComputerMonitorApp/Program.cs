f91f1e2843465de88b5633000f0dd5e779943d67
dongyeoh <yeoh@foxmail.com>
2025-12-15 09:54:22 +0800

    在程序启动时使用Mutex+程序集名称来判重，避免程序重复打开

--- ComputerMonitorApp/Program.cs
+++ ComputerMonitorApp/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Serilog;
+using Serilog.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using Serilog;
 
 namespace ComputerMonitorApp
 {
@@ -16,33 +19,48 @@ namespace ComputerMonitorApp
         [STAThread]
         static void Main()
         {
-            try
+            //避免重复打开多个实例
+            bool createNew;
+            var mutexName = Assembly.GetExecutingAssembly().GetName().Name;
+            using (Mutex mutex = new Mutex(true, mutexName, out createNew))
             {
-                //先初始化日志
-                LogManager.InitDefaultLogger();
-                Application.EnableVisualStyles();
-                //捕获所有异常避免程序崩溃
-                Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-                //主线程未捕获异常处理
-                Application.ThreadException += Application_ThreadException;
-                //全局异常处理
-                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-                //异步异常
-                TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new MainForm());
+                if (!createNew)
+                {
+                    MessageBox.Show("程序已经在运行中！");
+                    return;
+                }
+                try
+                {
+                    //先初始化日志
+                    LogManager.InitDefaultLogger();
+                    InternalMain();
+                }
+                catch (Exception e)
+                {
+                    //单独用一个日志文件存储程序启动时的错误
+                    File.AppendAllLines("start_error.txt",
+                        new String[] { DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":" + e.ToString() });
+                    MessageBox.Show("启动异常：" + e.Message);
+                }
+                finally
+                {
+                    LogManager.Close();
+                }
             }
-            catch(Exception e)
-            {
-                //单独用一个日志文件存储程序启动时的错误
-                File.AppendAllLines("start_error.txt",
-                    new String[] {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") +":"+ e.ToString() });
-                MessageBox.Show("启动异常："+e.Message);
-            }
-            finally
-            {
-                LogManager.Close();
-            }
+        }
+        private static void InternalMain()
+        {
+            Application.EnableVisualStyles();
+            //捕获所有异常避免程序崩溃
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            //主线程未捕获异常处理
+            Application.ThreadException += Application_ThreadException;
+            //全局异常处理
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //异步异常
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
         }
 
         private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
